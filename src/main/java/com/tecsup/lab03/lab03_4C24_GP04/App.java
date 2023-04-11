1dd461e87574021417dc0ad1ac4c1658d91517d3
Alumno <Alumno@LAB1507-07>
2023-04-11 13:50:43 -0500

    First commit

---
+++ src/main/java/com/tecsup/lab03/lab03_4C24_GP04/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.lab03_4C24_GP04;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
